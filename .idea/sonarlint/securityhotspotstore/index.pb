e444dd3c42ad579a0ac1fc5d661d66d62f351cbc
mehdi.dev <ramzimed302@gmail.com>
2023-11-02 15:35:20 +0100

    done

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,5 +3,7 @@
 app.py,1\3\13cce7fd076299c81b4986166f3d822791c9490e
 q
 Avenv/Lib/site-packages/customtkinter/windows/widgets/ctk_frame.py,a\f\af7c1931b41885397d655ac1b7aaa3d8b13fa854
-7
-test.py,a\3\a36e355ddb203d7d4133221f339dc406cb9f480f
+Å
+Q.idea/sonarlint/securityhotspotstore/a/3/a36e355ddb203d7d4133221f339dc406cb9f480f,e\a\ea27458825e414a205b30325b8b3cb470da2d13b
+w
+G.idea/sonarlint/issuestore/a/3/a36e355ddb203d7d4133221f339dc406cb9f480f,a\4\a426043e80ef068824c5b2743d0603b45b1c73ab
--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-
 import customtkinter as tk
 from tkinter import filedialog
 import pyttsx3
 from CTkMessagebox import CTkMessagebox
 from PIL import Image
-
-
 
 class TextToAudioConverterApp:
     def __init__(self, root):
@@ -15,10 +12,10 @@ class TextToAudioConverterApp:
         self.root.geometry("800x600")
         self.root.configure(bg="#333333")  # Dark background color
         tk.set_default_color_theme("green")
-        
+
         my_image = tk.CTkImage(light_image=Image.open("logo-v1.png"),
-                                          dark_image=Image.open("logo-v1.png"),
-                                          size=(150, 80))
+                               dark_image=Image.open("logo-v1.png"),
+                               size=(150, 80))
 
         self.logo_label = tk.CTkLabel(self.root, image=my_image, text="")
         self.logo_label.pack(padx=10, pady=5,)
@@ -40,22 +37,18 @@ class TextToAudioConverterApp:
         self.label_voice.pack(pady=(10, 5))
 
         self.radio_female = tk.CTkRadioButton(self.main_frame, text="Female", variable=self.selected_voice, value="female")
-        self.radio_female.pack(anchor=tk.CENTER,pady=(0, 10))
+        self.radio_female.pack(anchor=tk.CENTER, pady=(0, 10))
 
-        self.radio_male = tk.CTkRadioButton(self.main_frame, text="Male", variable=self.selected_voice, value="male",)
-        self.radio_male.pack(anchor=tk.CENTER,pady=(0, 10))
-
+        self.radio_male = tk.CTkRadioButton(self.main_frame, text="Male", variable=self.selected_voice, value="male")
+        self.radio_male.pack(anchor=tk.CENTER, pady=(0, 10))
 
         self.label_save = tk.CTkLabel(self.main_frame, text="Choose save location:", font=("Arial", 12), )
         self.label_save.pack(pady=(0, 5))
 
-        self.save_location = tk.CTkEntry(self.main_frame, width=400, height=30 )
+        self.save_location = tk.CTkEntry(self.main_frame, width=400, height=30)
         self.save_location.pack(pady=(0, 10),)
         self.browse_button = tk.CTkButton(self.main_frame, text="Browse", command=self.browse_save_location, )
         self.browse_button.pack(pady=(0, 10))
-
-        self.progressbar = tk.CTkProgressBar(self.main_frame, orientation="horizontal",width=400)
-        self.progressbar.pack(pady=(0, 10))
 
 
         self.convert_button = tk.CTkButton(self.main_frame, text="Convert", command=self.convert_text_to_audio, )
@@ -71,15 +64,16 @@ class TextToAudioConverterApp:
         save_path = self.save_location.get()
 
         if not text.strip():
-            CTkMessagebox(title="warning",message="Please enter text.", icon="warning", option_1="Ok")
-
+            CTkMessagebox(title="warning", message="Please enter text.", icon="warning", option_1="Ok")
             return
 
         if not save_path:
-            CTkMessagebox(title="warning", message="Please choose a save location.",  icon="warning", option_1="Ok")
+            CTkMessagebox(title="warning", message="Please choose a save location.", icon="warning", option_1="Ok")
             return
 
         try:
+
+
             engine = pyttsx3.init("sapi5")
             voices = engine.getProperty('voices')
             selected_voice = self.selected_voice.get()  # Get the actual value
@@ -89,12 +83,16 @@ class TextToAudioConverterApp:
             elif selected_voice == "male":
                 engine.setProperty("voice", voices[0].id)
 
-
             engine.save_to_file(text, save_path)
             engine.runAndWait()
-            CTkMessagebox(message="Text converted to audio successfully!",icon="check", option_1="close")
+
+            CTkMessagebox(message="Text converted to audio successfully!", icon="check", option_1="close")
+
         except Exception as e:
-            CTkMessagebox(title="Error",message=f"An error occurred: {str(e)}",icon="check", option_1="close")
+            CTkMessagebox(title="Error", message=f"An error occurred: {str(e)}", icon="warning", option_1="close")
+
+
+
 
 
 if __name__ == "__main__":
